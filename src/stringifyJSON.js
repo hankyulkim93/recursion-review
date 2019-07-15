56963909b13cab7fc0796a42970ff234a6f99946
hankyulkim93 <hankyulkim93@gmail.com>
2019-07-15 11:59:23 -0700

    Stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,23 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var holder;
+  if (typeof obj === "number" || typeof obj === "boolean") {
+    return obj.toString();
+  } else if (typeof obj === "string") {
+    return "\"" + obj + "\"";
+  } else if (obj === null) {
+    return "null";
+  } else if (obj === undefined) {
+    return undefined;
+  } else if (Array.isArray(obj) === true) {
+    holder = "[";
+    for (let i = 0; i < obj.length; i++) {
+      holder.push(obj[i]);
+    }
+    holder.push("]");
+    return holder + stringifyJSON(obj);
+  }
 };
+var example = [1, 2, [3], [[4,5]]];
+var example2 = "1, 2, [3], [4,5]";
