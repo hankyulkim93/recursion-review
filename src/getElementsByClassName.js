b662c6bf0cc800db2d8395f45f66cada1ce4da09
hankyulkim93 <hankyulkim93@gmail.com>
2019-07-15 20:25:55 -0700

    Removed outside recursive call

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,22 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  // declare holder variable 
+  // classList && classList.contains(className) --> holder.push(element)
+  // check for child elements, iterate through with a for loop, call function again (recursive case)
+  var result = [];
+  var current = window.document.body;
+  var findPlease = function(element,className) {
+    if (element.classList && element.classList.contains(className)) {
+      result.push(element);
+    }
+    if (element.hasChildNodes()) { // if has children
+      for (let i = 0; i < element.childNodes.length; i++) {
+        findPlease((element.childNodes[i]),className);
+      }
+    }
+  };
+  findPlease(current,className);
+  return result;
 };
+// getElementsByClassName("targetClassName");
